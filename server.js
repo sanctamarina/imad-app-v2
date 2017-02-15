18441412053256f1f914c5da60e7fad494b58ae7
sanctamarina <jsanctamarina@gmail.com>
2017-02-15 14:53:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res)
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-   res.send('this is my first article for webapp');
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+
    });
 
 app.get('/article-two', function (req, res) {
